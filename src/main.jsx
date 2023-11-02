31bf60b592647546e1c972f540065879ba4cd2f9
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2023-11-03 01:38:14 +0600

    redux-persist install and config

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,18 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Router.jsx";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
+import { persistor, store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <RouterProvider router={router}>
         <App />
       </RouterProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { api } from "./api/apiSlice";
 import cartReducer from "./features/cart/cartSlice";
 import tokenReducer from "./features/user/tokenSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["api"],
+};
+
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  userToken: tokenReducer,
+  [api.reducerPath]: api.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    userToken: tokenReducer,
-    [api.reducerPath]: api.reducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
 });
+
+export const persistor = persistStore(store)
