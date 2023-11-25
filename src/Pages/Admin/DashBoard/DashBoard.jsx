4879f631ff9fcaf108d4e581e67094eadf49afa3
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2023-11-25 22:55:02 +0600

    add coming soon to dashboard

--- src/Pages/Admin/DashBoard/DashBoard.jsx
+++ src/Pages/Admin/DashBoard/DashBoard.jsx
@@ -5,8 +5,8 @@ const DashBoard = () => {
   return (
     <div className="my-6 md:mx-3 ">
       <div className="grid xl:grid-cols-2 gap-8 lg:gap-12 ">
-        <div className="w-auto h-[300px] bg-blue-gray-50 shadow"></div>
-        <div className="w-auto h-[300px] bg-blue-gray-50 shadow"></div>
+        <div className="w-auto h-[300px] bg-blue-gray-50 shadow"> <p className="text-center">Coming Soon...</p> </div>
+        <div className="w-auto h-[300px] bg-blue-gray-50 shadow"> <p className="text-center">Coming Soon...</p> </div>
         <div className="w-auto h-auto mb-10">
           <div className="mb-6">
             <p className="text-3xl font-bold mb-2">Latest Order</p>
